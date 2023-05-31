2391955f2e374253659a134755c9772a367510ad
Peter Conenna <pconenna@depaul.edu>
2023-05-31 16:47:38 -0500

    wrote questions and first prompt

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+//project title, description, table of contents, installation, usage, contribution guidelines
+// test instructions 
+const questions = ["What is the title of the application?",
+"What is the description of the application?",
+"Write a table of contents.",
+"How does a user install the appllication?",
+"How does a user use the application?",
+"What are the contribution guidlines?",
+"Write test instructions."
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt([
+        {
+            type:'input',
+            message:questions[0],
+            name: 'title'
+
+        },
+    ])
+}
 
 // Function call to initialize app
 init();
